2f4c8f6d20c0dc688d9727ff3895904158fa9d46
poddubniydennis <dpoddubnyj471@gmail.com>
2026-06-16 11:50:53 +0300

    Задание с переменными и считыванием с консоли

---
+++ less_4/project_4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    float num , num1, num2;
+    scanf("%f", &num);
+    scanf("%f", &num1);
+    
+    num2 = num / num1;
+
+    printf("Variable = %f", num2);
+
+return 0;
+}
