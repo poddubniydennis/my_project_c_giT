dedfea319dac4d59a373460d06b4455c62da57e9
poddubniydennis <dpoddubnyj471@gmail.com>
2026-06-16 10:35:08 +0300

    Работа с переменными

---
+++ #2/project_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int num = 5;
+    int q = 3, r = 8;
+    int j;
+    
+   q += 1;
+
+    j = q + r;
+
+    printf("Variable J = %d", j);
+
+return 0;
+}
