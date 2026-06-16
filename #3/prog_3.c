d62ed8e20cf7f6073610c13a6c96dfe85d384055
poddubniydennis <dpoddubnyj471@gmail.com>
2026-06-16 11:16:32 +0300

    Работа с типами данных float

---
+++ #3/prog_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    float num = 5.5, num1 = 7.5, num2;
+    
+    num2 = num / num1;
+
+    printf("Variable = %f", num2);
+
+return 0;
+}
